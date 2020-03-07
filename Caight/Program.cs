3bfe74db0d11ac93f6a6a08f31fbd9357523dd79
nda111 <48427099+nda111@users.noreply.github.com>
2020-03-07 18:28:03 +0900

    FileSystem test
    create test.txt

--- Caight/Program.cs
+++ Caight/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,7 +16,25 @@ namespace Caight
     {
         public static void Main(string[] args)
         {
+            bool offSwitch = true;
+
+            Task.Run(() =>
+            {
+                int i = 0;
+                while (offSwitch)
+                {
+                    System.Diagnostics.Debug.WriteLine(i++);
+
+                    Thread.Sleep(1000);
+                }
+            });
+
+            FileStream stream = File.Create("test.txt");
+            stream.Write(new byte[] { 1, 2, 3, 4, 5 }, 0, 5);
+            stream.Close();
+
             CreateHostBuilder(args).Build().Run();
+            offSwitch = false;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
