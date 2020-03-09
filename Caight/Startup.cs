dbd031d9dd632b6b9e08dd4ffaf5ea6a9d7548a8
nda111 <48427099+nda111@users.noreply.github.com>
2020-03-09 14:46:56 +0900

    Test14
    File

--- Caight/Startup.cs
+++ Caight/Startup.cs
@@ -19,8 +19,34 @@ namespace Caight
 {
     public class Startup
     {
+        private string message = null;
         public Startup(IConfiguration configuration)
         {
+            string path = "test.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+                using (var stream = File.Open(path, FileMode.Open, FileAccess.Write))
+                {
+                    var writer = new StreamWriter(stream);
+                    writer.WriteLine(0);
+                    writer.Flush();
+                }
+            }
+
+            using (var stream = File.Open(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                var reader = new StreamReader(stream);
+                int cnt = int.Parse(reader.ReadLine()) + 1;
+
+                stream.Seek(0, SeekOrigin.Begin);
+                var writer = new StreamWriter(stream);
+                writer.WriteLine(cnt);
+                writer.Flush();
+
+                message = cnt + "\0";
+            }
+
             Configuration = configuration;
         }
 
@@ -55,7 +81,7 @@ namespace Caight
                 {
                     if (context.WebSockets.IsWebSocketRequest)
                     {
-                        WebSocket socket = await context.WebSockets.AcceptWebSocketAsync(); 
+                        WebSocket socket = await context.WebSockets.AcceptWebSocketAsync();
                         await Response(context, socket);
                     }
                     else
@@ -69,7 +95,7 @@ namespace Caight
                 }
             });
 
-            app.UseRouting(); 
+            app.UseRouting();
 
             app.UseAuthorization();
 
@@ -82,28 +108,24 @@ namespace Caight
         private async Task Response(HttpContext context, WebSocket socket)
         {
             var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result = null;
+            WebSocketReceiveResult result = null;// TODO: FIlesystem test
 
             while (!socket.CloseStatus.HasValue)
             {
-                // rec1
-                result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                string msg = Encoding.UTF8.GetString(buffer).Split('\0')[0];
                 using (var memStream = new MemoryStream(buffer))
                 {
                     var writer = new StreamWriter(memStream, Encoding.UTF8);
-                    writer.Write(msg + '\0');
+                    writer.Write(message);
+                    writer.Flush();
                 }
-                Console.WriteLine("Receive: " + msg);
 
-                // snd2
-                await socket.SendAsync(new ArraySegment<byte>(buffer), result.MessageType, result.EndOfMessage, CancellationToken.None);
-                Console.WriteLine("SendBack");
+                await socket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                Console.WriteLine("Send: " + message);
 
                 result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
 
-            await socket.CloseOutputAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
             Console.WriteLine("Close");
         }
     }
